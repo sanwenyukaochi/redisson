0049606da47d457906c7b5f9d9c5692f801bf966
sanwenyukaochi <sanwenyukaochi@outlook.com>
2025-12-17 18:40:14 +0800

    init

---
+++ project-spring/src/test/java/com/vinsguru/redisspring/RedisSpringApplicationTests.java
@@ -0,0 +1,14 @@
+package com.vinsguru.redisspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
